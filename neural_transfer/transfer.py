152c197b8c485a1a30943a6b7e44c4b260db50c3
YLyeliang <k87974@163.com>
2019-12-02 20:51:39 +0800

    some files

---
+++ neural_transfer/transfer.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import torch
+import torch.nn.functional as F
+
+a=tf.keras.losses.BinaryCrossentropy()
+tf.random.uniform()
+tf.summary.scalar()
+a=1
+c=1
